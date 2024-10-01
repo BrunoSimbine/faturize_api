fcb61a1970f08936b4d5ed0677de54244ea20740
Bruno Simbine <brunosimbine07012002@gmail.com>
2024-10-01 13:39:09 +0200

    Tela

--- Program.cs
+++ Program.cs
@@ -4,14 +4,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "MyPolicy",
+        policy => {
+            policy
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5228);
+});
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
